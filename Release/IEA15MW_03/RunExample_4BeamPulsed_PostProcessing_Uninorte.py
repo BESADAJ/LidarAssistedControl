fa8daeccd826238fba414eed658ad6047ae2efea
BESADAJ <besadaj@uninorte.edu.co>
2024-07-15 01:02:54 -0500

    brute force method in python

--- Release/IEA15MW_03/RunExample_4BeamPulsed_PostProcessing_Uninorte.py
+++ Release/IEA15MW_03/RunExample_4BeamPulsed_PostProcessing_Uninorte.py
@@ -33,65 +33,103 @@ R = 120
 # Parameter for Cost (Summer Games 2024)
 tau = 2
 
+#Parameters modification for brute force, T_Buffer, f_cutoff
+print("insert buffer start")
+T_buffer_start= float(input())
+T_buffer_reset = T_buffer_start
+print("insert buffer step")
+T_buffer_step= float(input())
+print("insert number of steps")
+T_buffer_count=int(input())
+print("insert f_cutoff start")
+f_cutoff_start= float(input())
+print("insert f_cutoff step")
+f_cutoff_step= float(input())
+print("insert number of steps")
+f_cutoff_count=int(input())
+CostPlot = np.zeros(T_buffer_count)
+BufferPlot = np.zeros(T_buffer_count)
 # Configuration from LDP_v1_4BeamPulsed.IN and LDP_v1_4BeamPulsed.IN
-LDP = {
-    "NumberOfBeams": 4,
-    "AngleToCenterline": 19.176,
-    "IndexGate": 6,
-    "FlagLPF": 1,
-    "f_cutoff": 0.1232,
-    "T_buffer": 1.3889,
-}
+for i_f_cutoff in range(f_cutoff_count):
+    T_buffer_start=T_buffer_reset
+    for iBuffer in range(T_buffer_count):
+        LDP = {
+            "NumberOfBeams": 4,
+            "AngleToCenterline": 19.176,
+            "IndexGate": 6,
+            "FlagLPF": 1,
+            "f_cutoff":  float(f_cutoff_start),
+            "T_buffer": float(T_buffer_start),
+        }
 
-# Files (should not be changed)
-SimulationFolderLAC = "SimulationResults_4BeamPulsed"
+        # Files (should not be changed)
+        SimulationFolderLAC = "SimulationResults_4BeamPulsed"
 
-# Allocate
-MAE = np.empty(nSeed)
+        # Allocate
+        MAE = np.empty(nSeed)
 
-# Loop over all seeds
-for iSeed in range(nSeed):
-    # Load data
-    Seed = Seed_vec[iSeed]
-    WindFileName = f'URef_18_Seed_{Seed:02d}'
-    FASTresultFile = f'{SimulationFolderLAC}/{WindFileName}_FlagLAC_1.outb'
-    FBFF = ReadFASTbinaryIntoStruct(FASTresultFile)
+        # Loop over all seeds
 
-    # Calculate REWS
-    R_FBFF = CalculateREWSfromLidarData_LDP_v1(FBFF, DT, TMax, LDP)
+        for iSeed in range(nSeed):
+            # Load data
+            Seed = Seed_vec[iSeed]
+            WindFileName = f'URef_18_Seed_{Seed:02d}'
+            FASTresultFile = f'{SimulationFolderLAC}/{WindFileName}_FlagLAC_1.outb'
+            FBFF = ReadFASTbinaryIntoStruct(FASTresultFile)
 
-    # Get REWS from the wind field and interpolate it on the same time vector
-    TurbSimResultFile = f'TurbulentWind/URef_18_Seed_{Seed:02d}.wnd'
-    REWS_WindField, Time_WindField = CalulateREWSfromWindField(TurbSimResultFile, R, 2)
-    REWS_WindField_Fs = interp1d(Time_WindField.ravel(), REWS_WindField.ravel())(R_FBFF['Time'])
+            # Calculate REWS
+            R_FBFF = CalculateREWSfromLidarData_LDP_v1(FBFF, DT, TMax, LDP)
 
-    # Calculate mean absolute error
-    REWS_WindField_Fs_shifted = interp1d(Time_WindField.ravel() - tau, REWS_WindField.ravel())(R_FBFF['Time'])
-    Error = REWS_WindField_Fs_shifted - R_FBFF["REWS_b"]
-    MAE[iSeed] = np.mean(np.abs(Error[R_FBFF["Time"] >= t_start]))
+            # Get REWS from the wind field and interpolate it on the same time vector
+            TurbSimResultFile = f'TurbulentWind/URef_18_Seed_{Seed:02d}.wnd'
+            REWS_WindField, Time_WindField = CalulateREWSfromWindField(TurbSimResultFile, R, 2)
+            REWS_WindField_Fs = interp1d(Time_WindField.ravel(), REWS_WindField.ravel())(R_FBFF['Time'])
 
-    # Plot REWS for absolute error
-    plt.figure('REWS seed {}'.format(Seed))
-    plt.subplot(311)
-    plt.plot(R_FBFF['Time'], REWS_WindField_Fs, label='wind field')
-    plt.plot(R_FBFF['Time'], R_FBFF['REWS'], label='lidar estimate')
-    plt.ylabel('REWS [m/s]')
-    plt.legend()
-    plt.grid(True)
-    plt.subplot(312)
-    plt.plot(R_FBFF['Time'], REWS_WindField_Fs_shifted, label='wind field shifted')
-    plt.plot(R_FBFF['Time'], R_FBFF['REWS_b'], label='lidar estimate filtered and buffered')
-    plt.ylabel('REWS [m/s]')
-    plt.legend()
-    plt.grid(True)
-    plt.subplot(313)
-    plt.plot(R_FBFF['Time'], Error)
-    plt.ylabel('error [m/s]')
-    plt.xlabel('time [s]')
-    plt.grid(True)
+            # Calculate mean absolute error
+            REWS_WindField_Fs_shifted = interp1d(Time_WindField.ravel() - tau, REWS_WindField.ravel())(R_FBFF['Time'])
+            Error = REWS_WindField_Fs_shifted - R_FBFF["REWS_b"]
+            MAE[iSeed] = np.mean(np.abs(Error[R_FBFF["Time"] >= t_start]))
 
+            # Plot REWS for absolute error
+            # plt.figure('REWS seed {}'.format(Seed))
+            # plt.subplot(311)
+            # plt.plot(R_FBFF['Time'], REWS_WindField_Fs, label='wind field')
+            # plt.plot(R_FBFF['Time'], R_FBFF['REWS'], label='lidar estimate')
+            # plt.ylabel('REWS [m/s]')
+            # plt.legend()
+            # plt.grid(True)
+            # plt.subplot(312)
+            # plt.plot(R_FBFF['Time'], REWS_WindField_Fs_shifted, label='wind field shifted')
+            # plt.plot(R_FBFF['Time'], R_FBFF['REWS_b'], label='lidar estimate filtered and buffered')
+            # plt.ylabel('REWS [m/s]')
+            # plt.legend()
+            # plt.grid(True)
+            # plt.subplot(313)
+            # plt.plot(R_FBFF['Time'], Error)
+            # plt.ylabel('error [m/s]')
+            # plt.xlabel('time [s]')
+            # plt.grid(True)
+        BufferPlot[iBuffer] = T_buffer_start
+        T_buffer_start = T_buffer_start + T_buffer_step
+        Cost = np.mean(MAE)
+
+
+
+
+        print(f'Cost for Summer Games 2024 ("18 m/s hurdles"): {Cost:.6f}')
+
+        CostPlot[iBuffer]= Cost
+
+
+
+    plt.plot(BufferPlot,CostPlot,label=f_cutoff_start )
+    plt.legend(title= "f_cutoff [Hz]")
+    f_cutoff_start = f_cutoff_start + f_cutoff_step
+
+
+plt.ylabel('Cost')
+plt.xlabel('T_Buffer')
 plt.show()
 
+
 # Calculation of Cost for Summer Games 2024
-Cost = np.mean(MAE)
-print(f'Cost for Summer Games 2024 ("18 m/s hurdles"): {Cost:.6f}')
